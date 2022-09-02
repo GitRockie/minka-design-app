4a82c7346111433c109e7eb5696c2bf4cab4f9fb
Roman Kostyuchenko <elromankos@gmail.com>
2022-09-02 18:35:06 +0200

    Main.dart works
    InitialRoutes and Routes Create
    Basic Design Screen
    Open Assets in pubspec
    Import photos
    screen design
    Title() extract

---
+++ lib/source/screens/basic_design.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class BasicDesignScreen extends StatelessWidget {
+  const BasicDesignScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: const [
+        Image(
+          image: AssetImage('assets/port-bay.jpg'),
+        ),
+
+        //Title Widget
+        Title(),
+      ],
+    ));
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'TMA | Puerto de Barcelona',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Barcelona, Spain',
+                style: TextStyle(color: Colors.black45),
+              ),
+            ],
+          ),
+          Expanded(child: Container()),
+          const Icon(Icons.star, color: Colors.red),
+          const Text('42'),
+        ],
+      ),
+    );
+  }
+}
