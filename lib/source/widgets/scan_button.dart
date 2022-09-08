8adc9fbe6c4987c9527cd1651b69a4a05790983f
Roman Kostyuchenko <elromankos@gmail.com>
2022-09-08 23:18:18 +0200

    Scan Mode
    barcodeScanRes  on button set
     flutter_barcode_scanner: ^2.0.0
    pubspec.yaml

--- lib/source/widgets/scan_button.dart
+++ lib/source/widgets/scan_button.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({super.key});
@@ -9,6 +12,16 @@ class ScanButton extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.teal,
         child: const Icon(Icons.qr_code_scanner_outlined),
-        onPressed: () {});
+        onPressed: () async {
+          try {
+            String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+                "#F04F33", "Cancel", false, ScanMode.QR);
+
+            print(barcodeScanRes);
+          } on PlatformException catch (e) {
+//never calling
+            throw Exception(e.message);
+          }
+        });
   }
 }
