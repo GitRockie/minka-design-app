a19e5eba1bead9e5614613cd07859254c5a201e0
Roman Kostyuchenko <elromankos@gmail.com>
2022-09-17 02:03:23 +0200

    launchURL corrections
    OK

--- lib/source/utils/utils.dart
+++ lib/source/utils/utils.dart
@@ -4,12 +4,12 @@ import 'package:url_launcher/url_launcher.dart';
 import '../models/scan_model.dart';
 
 launchURL(BuildContext context, ScanModel scan) async {
-  final Uri url = Uri.parse(scan.value);
+  final url = scan.value;
 
   if (scan.type == 'http') {
     //Open Website
-    if (await canLaunchUrl(url)) {
-      await canLaunchUrl(url);
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url), mode: LaunchMode.inAppWebView);
     } else {
       throw 'Could not launch $url';
     }
