0419b0a773058aa8c932a9677df02d888d0c2390
Roman Kostyuchenko <elromankos@gmail.com>
2022-09-24 19:49:37 +0200

    Input styling to re-use
    InputDecorations  class create in UI directory
    Arguments
    required String hintText,
    required String labelText, IconData? prefixIcon

--- lib/source/screens/login_screen.dart
+++ lib/source/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minka_design_app/source/ui/input_decotations.dart';
 import 'package:minka_design_app/source/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -35,7 +36,10 @@ class LoginScreen extends StatelessWidget {
           const Text(
             'Create new account',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          )
+          ),
+          const SizedBox(
+            height: 100,
+          ),
         ],
       )),
     ));
@@ -53,20 +57,39 @@ class _LoginForm extends StatelessWidget {
             TextFormField(
                 autocorrect: false,
                 keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.deepPurple)),
-                  focusedBorder: UnderlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Colors.deepPurple, width: 2)),
-                  hintText: 'minker.user@gmail.com',
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Colors.grey),
-                  prefixIcon: Icon(
-                    Icons.alternate_email_sharp,
-                    color: Colors.deepPurple,
-                  ),
-                ))
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: 'minker.user@gmail.com',
+                    labelText: 'Email',
+                    prefixIcon: Icons.alternate_email_sharp)),
+            const SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+                autocorrect: false,
+                obscureText: true,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: '******',
+                    labelText: 'Password',
+                    prefixIcon: Icons.lock_clock_sharp)),
+            const SizedBox(
+              height: 50,
+            ),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                disabledColor: Colors.grey,
+                elevation: 0,
+                color: Colors.deepPurple,
+                child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 80, vertical: 15),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white),
+                    )),
+                onPressed: () {})
           ],
         ),
       ),
