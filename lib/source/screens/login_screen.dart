73b05ff52f5f527e367287c35ad19d577a80c101
Roman Kostyuchenko <elromankos@gmail.com>
2022-09-25 22:43:41 +0200

    HTTP Request Simulating
    Private/Public Getters/Setters create in login_form_provider
    notifyListeners() set
    Working with Button onPressed
    FocusScope.of(context).unfocus();
     await Future.delayed
    Text changing

--- lib/source/screens/login_screen.dart
+++ lib/source/screens/login_screen.dart
@@ -56,71 +56,78 @@ class _LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final loginForm = Provider.of<LoginFormProvider>(context);
-    return Container(
-      child: Form(
-        key: loginForm.formKey,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        child: Column(
-          children: [
-            TextFormField(
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecorations.authInputDecoration(
-                  hintText: 'minker.user@gmail.com',
-                  labelText: 'Email',
-                  prefixIcon: Icons.alternate_email_sharp),
-              onChanged: (value) => loginForm.email = value,
-              validator: (value) {
-                String pattern =
-                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-                RegExp regExp = RegExp(pattern);
-                return regExp.hasMatch(value ?? '')
-                    ? null
-                    : 'Email is incorrect';
-              },
+    return Form(
+      key: loginForm.formKey,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Column(
+        children: [
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecorations.authInputDecoration(
+                hintText: 'minker.user@gmail.com',
+                labelText: 'Email',
+                prefixIcon: Icons.alternate_email_sharp),
+            onChanged: (value) => loginForm.email = value,
+            validator: (value) {
+              String pattern =
+                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+              RegExp regExp = RegExp(pattern);
+              return regExp.hasMatch(value ?? '') ? null : 'Email is incorrect';
+            },
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          TextFormField(
+            autocorrect: false,
+            obscureText: true,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecorations.authInputDecoration(
+                hintText: '******',
+                labelText: 'Password',
+                prefixIcon: Icons.lock_clock_sharp),
+            onChanged: (value) => loginForm.password = value,
+            validator: (value) {
+              return (value != null && value.length >= 6)
+                  ? null
+                  : 'The password is incorrect';
+            },
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          MaterialButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
             ),
-            const SizedBox(
-              height: 30,
-            ),
-            TextFormField(
-              autocorrect: false,
-              obscureText: true,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecorations.authInputDecoration(
-                  hintText: '******',
-                  labelText: 'Password',
-                  prefixIcon: Icons.lock_clock_sharp),
-              onChanged: (value) => loginForm.password = value,
-              validator: (value) {
-                return (value != null && value.length >= 6)
-                    ? null
-                    : 'The password is incorrect';
-              },
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            MaterialButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                disabledColor: Colors.grey,
-                elevation: 0,
-                color: Colors.deepPurple,
-                child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 80, vertical: 15),
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(color: Colors.white),
-                    )),
-                onPressed: () {
-                  if (!loginForm.isValidForm()) return;
-                  Navigator.pushReplacementNamed(
-                      context, HomeScreen.routerName);
-                })
-          ],
-        ),
+            disabledColor: Colors.grey,
+            elevation: 0,
+            color: Colors.deepPurple,
+            onPressed: loginForm.isLoading
+                ? null
+                : () async {
+                    FocusScope.of(context).unfocus();
+                    if (!loginForm.isValidForm()) return;
+
+                    loginForm.isLoading = true;
+
+                    await Future.delayed(const Duration(seconds: 2));
+                    //TODO: validate correct login
+                    loginForm.isLoading = false;
+
+                    Navigator.pushReplacementNamed(
+                        context, HomeScreen.routerName);
+                  },
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                child: Text(
+                  loginForm.isLoading ? 'Loading...' : 'Login',
+                  style: const TextStyle(color: Colors.white),
+                )),
+          )
+        ],
       ),
     );
   }
